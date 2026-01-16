287da46b3b0ed1ffa5a2ae8f8d36118915f81fbe
GitHub Action <action@github.com>
2026-01-16 08:35:04 +0000

    Update run-mcp to v1.0.3

--- Formula/run-mcp.rb
+++ Formula/run-mcp.rb
@@ -7,20 +7,20 @@ class RunMcp < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/serverless-dna/run-mcp/releases/download/v1.0.3/run-mcp-darwin-arm64"
-      sha256 "e6e9e47f0244ea2e83d802a7b9bfbfabf90ca60e5e960601ad841c337fa668bd"
+      sha256 "70f7cc468cdb03c346478bdce14fa24f0218f8a3d695243b5a8f726c7180b8a3"
     else
       url "https://github.com/serverless-dna/run-mcp/releases/download/v1.0.3/run-mcp-darwin-amd64"
-      sha256 "c50ef8b4e4b60cca539026b63e9286d51e730b092d404a5af95257ab8fd6e1da"
+      sha256 "ec82aa6e8698db298b2aa0f6eeeaf9936072b06294ab38ff6762e0807a365749"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/serverless-dna/run-mcp/releases/download/v1.0.3/run-mcp-linux-arm64"
-      sha256 "8323a12ae809e9971f8191132ccae477911106169aeb6c89b8ee7b2f3dcc6575"
+      sha256 "83a16f0f422cf6a91990b137ec8ae725e17db79ff1a7a6afa0cb2de9f6693199"
     else
       url "https://github.com/serverless-dna/run-mcp/releases/download/v1.0.3/run-mcp-linux-amd64"
-      sha256 "65c6780b3679ff2debd6f6a89222d37f04fc2c6e49064b57f017e9bbab70a91b"
+      sha256 "5ceb7d7916fc152f3ebadc32bd86a88cc5c5fbafd13ec159b4ad72050523ba1c"
     end
   end
   
